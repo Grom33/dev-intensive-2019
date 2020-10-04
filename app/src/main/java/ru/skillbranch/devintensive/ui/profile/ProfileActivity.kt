94fde1d0b6168d92fffefe29b66f632d984407ce
Gromov Vitaly <grom33@mail.ru>
2020-10-04 23:13:48 +0300

    add properties

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -0,0 +1,26 @@
+package ru.skillbranch.devintensive.ui.profile
+
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_profile.*
+import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.Bender
+
+class ProfileActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+    }
+}
